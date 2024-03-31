b8cb9fd34785503036af235df632a447b5758336
Ligzus <153594049+Ligzus@users.noreply.github.com>
2024-03-31 18:12:36 +0300

    верстка add-post-psge готова

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,13 +1,40 @@
+// Добавляем посты:
+import { renderHeaderComponent } from "./header-component.js";
+
+
+
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   const render = () => {
     // TODO: Реализовать страницу добавления поста
     const appHtml = `
     <div class="page-container">
       <div class="header-container"></div>
-      Cтраница добавления поста
-      <button class="button" id="add-button">Добавить</button>
-    </div>
-  `;
+
+      <div class="form">      
+        <h1 class="form-title">Новая запись:</h1>
+
+        <div class="form-inputs">
+          <p class="post-text">Описание:</p> 
+          <input type="text" id="add-text" class="textarea"/>
+
+          <label class="file-upload-label secondary-button">
+            <input
+              value=""
+              id="choose-photo"
+              type="file"
+              class="file-upload-input"
+              style="display:none"
+            > Открыть галерею
+          </label>   
+                          
+          <button class="button" id="add-button">
+            Добавить
+          </button>
+        </div>
+
+      </div>
+
+    </div> `;
 
     appEl.innerHTML = appHtml;
 
@@ -19,5 +46,6 @@ export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
     });
   };
 
-  render();
-}
+  render();    
+  renderHeaderComponent({ element: document.querySelector(".header-container") });
+};
